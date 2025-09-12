8a301224da364e386f8b766e095a37ea121e7dce
Johannes Spangenberg <johannes.spangenberg@hotmail.de>
2025-09-13 00:40:21 +0200

    Restructure pyproject.toml, remove discouraged options

--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-# This file exists only for backward compatibility.
-# All configuration is stored in pyproject.toml,
-# but some tools may still require the existence of setup.py.
-setup()
